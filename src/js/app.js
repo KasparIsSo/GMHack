20fbd7319cf891a65470441d5d2ba0fe1cc37165
kshen3778 <kshen3778@gmail.com>
2017-06-24 16:28:21 -0400

    third

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,74 @@
 // Your code goes here
 
 var vinElem = document.getElementById('vin');
-gm.info.getVehicleConfiguration(function(data) {
+/*gm.info.getVehicleConfiguration(function(data) {
   vinElem.innerHTML = gm.info.getVIN();
+  //console.log("hi");
+});*/
+
+
+console.log("hello");
+
+var divRoot = $("#affdex_elements")[0];
+//var divRoot = document.getElementById('affdex_elements')[0];
+// The captured frame's width in pixels
+var width = 640; //640
+
+// The captured frame's height in pixels
+var height = 480; //480
+
+/*
+   Face detector configuration - If not specified, defaults to
+   affdex.FaceDetectorMode.LARGE_FACES
+   affdex.FaceDetectorMode.LARGE_FACES=Faces occupying large portions of the frame
+   affdex.FaceDetectorMode.SMALL_FACES=Faces occupying small portions of the frame
+*/
+
+console.log(width + "," + height);
+var faceMode = affdex.FaceDetectorMode.LARGE_FACES;
+
+//Construct a CameraDetector and specify the image width / height and face detector mode.
+var detector = new affdex.CameraDetector(divRoot, width, height, faceMode);
+
+detector.detectAllExpressions();
+detector.detectAllEmotions();
+detector.detectAllEmojis();
+detector.detectAllAppearance();
+
+console.log(detector);
+
+detector.addEventListener("onInitializeSuccess", function() {
+  console.log("success");
+});
+detector.addEventListener("onInitializeFailure", function() {
+  console.log("failure");
+});
+detector.addEventListener("onWebcamConnectSuccess", function() {
+	console.log("I was able to connect to the camera successfully.");
+});
+
+detector.addEventListener("onWebcamConnectFailure", function() {
+	console.log("I've failed to connect to the camera :(");
+});
+detector.start();
+
+detector.addEventListener("onImageResultsSuccess", function (faces, image, timestamp) {
+  console.log(faces.length);
+
+  if (faces.length > 0) {
+    console.log(faces[0].emotions);
+
+        console.log("Anger: " + faces[0].emotions.anger);
+
+        vinElem.innerHTML = ""+faces[0].emotions.anger;
+          //console.log("hi");
+
+      //alert("angry");
+    //vinElem.innerHTML = "";
+  }
+});
+
+
+detector.addEventListener("onImageResultsFailure", function (image, timestamp, err_detail) {
+  console.log(err_detail);
 });
